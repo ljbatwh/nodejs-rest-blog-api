a847cd9ac36274f52db6ca9fa980073c850569ae
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2019-10-08 22:51:18 +0300

    Install cors package to handle cors errors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 //Configure dotenv package
 dotenv.config();
@@ -29,18 +30,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Handling CORS errors
-app.use((req, res, next) => {
-  req.header("Access-Control-Allow-Origin", "*");
-  req.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, GET, DELETE");
-    res.status(200).json({});
-  }
-  next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   req.header("Access-Control-Allow-Origin", "*");
+//   req.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, GET, DELETE");
+//     res.status(200).json({});
+//   }
+//   next();
+// });
 
 //Routes whitch should handle requests
 
